f0ae8271031d8b661576b85148304de40b525d80
Rinat <eenot84@yandex.ru>
2022-07-23 18:31:45 +0300

    first commit

---
+++ src/test/java/com/example/shareit/ShareItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shareit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
